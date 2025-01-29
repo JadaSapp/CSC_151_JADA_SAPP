d3991cad7805e98c1fc4ff6ce36d67ca9518d4bc
SANDHILLS\LH218-08 <jad.talbert@gmail.com>
2025-01-29 11:16:09 -0500

    Java Work

--- src/labs/example/hello/Mammal.java
+++ src/labs/example/hello/Mammal.java
@@ -24,54 +24,54 @@ public void setName (String name){
 }
 
  public void run(){
-
+    System.out.println(this.name + "is running, where to?")
   }
 
  public void sleep (){
-
+    System.out.println(this.name + "is asleep. peaceful dreams.")
  }
 
 public void scratch(){
-
+    System.out.println(this.name + "is scratching, be careful!")
 }
 
 public void fly(){
-
+    System.out.println(this.name + "is flying, the places they can go!")
 }
 
 public void hunt(){
-
+    System.out.println(this.name + "is hunting, make sure to not be spotted!")
 }
 
 public void stalk(){
-
+    System.out.println(this.name + "is stalking, they will now move slow and carefully.")
 }
 
 public void locate(){
-
+    System.out.println(this.name + "is locating, who are they trying to find?")
 }
 
 public void migrate(){
-
+    System.out.println(this.name + "is migrating, where will be their next home?")
 }
 
 public void pollinate(){
-
+    System.out.println(this.name + "is pollinating, time to protect the plants!")
 }
 
 public void swim(){
-
+    System.out.println(this.name + "is swimming, the water can allow you to travel far!")
 }
 
 public void haul(){
-
+    System.out.println(this.name + "is hauling, their body temperature is beginning to regulate. ")
 }
 
 public void molt(){
-
+    System.out.println(this.name + "is molting, their fur will stay healthy!")
 }
 
 public void dive(){
-
+    System.out.println(this.name + "is diving, what food will they find?")
 }
  }   
