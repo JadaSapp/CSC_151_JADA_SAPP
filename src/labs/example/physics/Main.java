05e278c3af2fdeace688bf27f347213e4a44b9ee
SANDHILLS\LH218-08 <jad.talbert@gmail.com>
2025-02-12 11:48:47 -0500

    python week 5

--- src/labs/example/physics/Main.java
+++ src/labs/example/physics/Main.java
@@ -4,4 +4,9 @@
 @purpose: Creating a main file with methods and equations
 */
 
-import java.lang.Math
+import java.lang.Math;
+
+public class Main {
+    public static void main(String[] args)
+
+}
--- src/labs/example/physics/Physics.java
+++ src/labs/example/physics/Physics.java
@@ -4,4 +4,39 @@
 @purpose: Psychics Java Class
 */
 
-import java.lang.Math
+import java.lang.Math;
+
+public class Physics { 
+
+    public double getDistance (double inital velocity, double time){
+        return
+    }
+
+    public double getVelocity (double){
+        return
+    }
+
+    public double getMomentum (double momentum, double velocity){
+        return
+    }
+
+    public double getForce (double momentum){
+        return
+    }
+
+    public double getWork (double force, double distance){
+        return
+    }
+
+    public double getKineticEnergy (double mass1, double mass2, double distance){
+        return
+    }
+
+    public double getPotentialEnergy (double mass, double force){
+        return
+    }
+
+    public double gravity (double momentum){
+        return
+    }
+}
