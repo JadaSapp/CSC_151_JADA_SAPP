f3f8372e5d64542a364fb5252a03f3babae2df64
SANDHILLS\LH218-08 <jad.talbert@gmail.com>
2025-02-13 10:32:17 -0500

    Java Program Week 5

--- src/labs/example/physics/Main.java
+++ src/labs/example/physics/Main.java
@@ -4,9 +4,19 @@
 @purpose: Creating a main file with methods and equations
 */
 
-import java.lang.Math;
+package labs.example.physics;
 
 public class Main {
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        Physics physics = new Physics();
+
+        System.out.println("The distance would be: " + physics.getDistance(3,9) + "miles");
+        System.out.println("The velocity would be: " + physics.getVelocity(9,10) + "mph");
+        System.out.println("The momentum would be: " + physics.getMomentum(20,1) + "kg m/s");
+        System.out.println("The force would be: " + physics.getForce(2,9) + "mg m/s^2");
+        System.out.println("The work would be: " + physics.getForce(9,8) + "Joules");
+        System.out.println("The kinetic energy would be: " + physics.getKineticEnergy(6,5) + "Joules");
+        System.out.println("The potential energy would be: " + physics.getPotentialEnergy(4,8) + "Joules");
+    }
 
 }
