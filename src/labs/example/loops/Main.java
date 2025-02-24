ce80c9b031020c6961ee59be182e87a619491d1d
SANDHILLS\LH218-08 <jad.talbert@gmail.com>
2025-02-24 11:37:11 -0500

    Lab 7 Work

---
+++ src/labs/example/loops/Main.java
@@ -0,0 +1,7 @@
+/*
+@author: Jada Sapp
+@Date: 2/24/2025
+@purpose: Main class for WhileLoop.java
+*/
+
+package labs.example.loops;
---
+++ src/labs/example/loops/WhileLoop.java
@@ -0,0 +1,7 @@
+/*
+@author: Jada Sapp
+@Date: 2/24/2025
+@purpose: Creating loops
+*/
+
+package labs.example.loops;
