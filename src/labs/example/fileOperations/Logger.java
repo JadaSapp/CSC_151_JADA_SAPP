e1c5a2a7830f065bf7511ee1df21728fb2d36baa
Sethjpurvis <purvissj403@mail.sandhills.edu>
2025-04-03 10:25:00 -0400

    Fixed Java Week 12 Errors

--- src/labs/example/fileOperations/Logger.java
+++ src/labs/example/fileOperations/Logger.java
@@ -12,7 +12,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class Logger{
-    private static final String FILE_PATH = "T:/CSC_151_JADA_SAPP/src/labs/example/fileOperations/FileOperator.java";
+    private static final String FILE_PATH = "T:/CSC_151_JADA_SAPP/src/labs/example/fileOperations/";
     private static final String ERROR_FILE = FILE_PATH + "logs/api_error.log";
 
     public static void main(String[] args) throws IOException{
@@ -37,63 +37,60 @@ public class Logger{
                 br.close();
             }
         }
-        catch(IOException e){
+        catch(Exception e){
             System.out.println(e);
         }
     }
 
-    private static BufferedReader openErrorLog() throws IOException{
-        BufferedReader br = new BufferedReader(new FileReader(ERROR_FILE));
+    public static BufferedReader openErrorLog() throws IOException{
+    BufferedReader br = new BufferedReader(new FileReader(ERROR_FILE));
 
-        return br;
+    return br;
     }
 
     private static void getCountOfErrorTypes(BufferedReader file) throws IOException{
-        int errors = 0;
-        int warns = 0;
-        int infos = 0;
-        int debugs = 0;
+        int error_count = 0;
+        int warn_count = 0;
+        int info_count = 0;
+        int debug_count = 0;
         String line = "";
 
         while((line = file.readLine()) != null){
             String[] lineValue = line.split(" ");
             switch(lineValue[2]){
                 case "[ERROR]":
-                    errors = errors + 1;
+                    error_count = error_count + 1;
                     break;
                 case "[WARN]":
-                    warns = warns + 1;
+                    warn_count = warn_count + 1;
                     break;
                 case "[INFO]":
-                    infos = infos + 1;
+                    info_count = info_count + 1;
                     break;
                 case "[DEBUG]":
-                    debugs = debugs + 1;
+                    debug_count = debug_count + 1;
                     break;
             }
         }
-        System.out.println("Total Amount of Errors is " + errors);
-        System.out.println("Total Amount of Warns is " + warns);
-        System.out.println("Total Amount of Infos is " + infos);
-        System.out.println("Total Amount of Debugs is " + debugs);
+        System.out.println("Total Amount of Errors is " + error_count);
+        System.out.println("Total Amount of Warns is " + warn_count);
+        System.out.println("Total Amount of Infos is " + info_count);
+        System.out.println("Total Amount of Debugs is " + debug_count);
     }
 
     private static void getMemoryLimitExceededCount(BufferedReader file) throws IOException{
         ArrayList <String> endPointList = new ArrayList<String>();
-        int getMemoryLimitExceededCount = 0;
-        String ling = "";
+        int memoryLimitExceededCount = 0;
+        String line = "";
 
         while((line = file.readLine()) != null){
             String[] lineValue = line.split(" ");
-            System.out.println(lineValue[5]);
-
             if(lineValue[5].equals("Memory")){
                 memoryLimitExceededCount = memoryLimitExceededCount + 1;
-                System.out.println(lineValue[13]);
                 endPointList.add(lineValue[13]);
             }
         }
-        System.out.println("Memory Limit Exceeded Count: " + memoryLimitExceededCount);
+        System.out.println("Memory Limit Exceeded Count is: " + memoryLimitExceededCount);
         for(String endPoint: endPointList){
             System.out.println(endPoint);
         }
