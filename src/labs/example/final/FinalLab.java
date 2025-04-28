cbe9d068b774c148c3ec580a85194b9672f44414
SANDHILLS\LH218-08 <jad.talbert@gmail.com>
2025-04-28 11:46:37 -0400

    Final Lab Assignment WIP

---
+++ src/labs/example/final/FinalLab.java
@@ -0,0 +1,30 @@
+/*
+@author: Jada Sapp
+@Date: 4/28/2025
+@purpose: Final Lab Assignment
+*/
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({ apiKey: "YOUR_API_KEY" });
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log(response.text);
+}
+
+await main();
